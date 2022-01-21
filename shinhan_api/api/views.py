15552c4eab732795687d5bef140582ee7c0cd9f7
andarabr <andarabrey@gmail.com>
2022-01-21 16:15:57 +0700

    first commit

---
+++ shinhan_api/api/views.py
@@ -0,0 +1,67 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Item
+from .serializers import ItemSerializer
+from rest_framework import serializers
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+@api_view(['GET'])
+def ApiOverview(request):
+    api_urls = {
+        'all_items': '/',
+        'Search by Category': '/?category=category_name',
+        'Search by Subcategory': '/?subcategory=category_name',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/item/pk/delete'
+    }
+  
+    return Response(api_urls)
+
+@api_view(['POST'])
+def add_items(request):
+    item = ItemSerializer(data=request.data)
+  
+    # validating for already existing data
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+  
+    if item.is_valid():
+        item.save()
+        return Response(status=status.HTTP_201_CREATED, data=item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def view_items(request):
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = Item.objects.filter(**request.query_params.dict())
+    else:
+        items = Item.objects.all()
+  
+    # if there is something in items else raise error
+    if items:
+        serializer = ItemSerializer(items, many=True)
+        print(serializer)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PUT'])
+def update_items(request, pk):
+    item = Item.objects.get(pk=pk)
+    serializer = ItemSerializer(instance=item, data=request.data)
+  
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['DELETE'])
+def delete_items(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    item.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
